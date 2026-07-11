57919db572097d250c8745b5e2ab41b3af1f29f2
Kha <khakee1234@gmail.com>
2026-07-11 14:36:24 +0700

    9.3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -42,6 +42,8 @@ const events = ref<Event[]>([
 </script>
 
 <template>
+  <h1>Events For Good</h1>
+
   <div class="events">
     <!-- <EventCard v-for="event in events" :key="event.id" :event="event" /> -->
     <div v-for="event in events" :key="event.id" class="event-wrapper">
