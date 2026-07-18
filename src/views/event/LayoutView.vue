b060169917874dc9d4b24bdbafdfffd0d91ff891
Kha <khakee1234@gmail.com>
2026-07-18 23:01:17 +0700

    5.2 (no underline)

--- src/views/event/LayoutView.vue
+++ src/views/event/LayoutView.vue
@@ -18,11 +18,15 @@ onMounted(() => {
     .then((response) => {
       event.value = response.data
     })
-    .catch(() => {
-      router.push({
-        name: '404-resource-view',
-        params: { resource: 'event' },
-      })
+    .catch((error) => {
+      if (error.response && error.response.status === 404) {
+        router.push({
+          name: '404-resource-view',
+          params: { resource: 'event' },
+        })
+      } else {
+        router.push({ name: 'network-error-view' })
+      }
     })
 })
 </script>
