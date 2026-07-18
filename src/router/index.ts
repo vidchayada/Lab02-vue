a391fd5760a1374d17a85abe530b00749b3226e8
Kha <khakee1234@gmail.com>
2026-07-18 22:32:37 +0700

    3.3

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
-import EventDetailView from '@/views/EventDetailView.vue'
+import EventDetailView from '@/views/event/DetailView.vue'
+import EventRegisterView from '@/views/event/RegisterView.vue'
+import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
 import StudentListView from '@/views/StudentListView.vue'
 
 const router = createRouter({
@@ -18,9 +21,26 @@ const router = createRouter({
     },
     {
       path: '/event/:id',
-      name: 'event-detail-view',
-      component: EventDetailView,
+      name: 'event-layout-view',
+      component: EventLayoutView,
       props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+        },
+      ],
     },
     {
       path: '/about',
