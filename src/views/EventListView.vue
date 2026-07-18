edf81addc3d1d8a256f5de037600f2c31d37ccd0
Kha <khakee1234@gmail.com>
2026-07-18 20:55:37 +0700

    2.9 (adding as query parameter-editing on eventlistview^index)

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -2,19 +2,34 @@
 import EventCard from '@/components/EventCard.vue'
 import EventInfo from '@/components/EventInfo.vue'
 import type { Event } from '@/types'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
 
 const events = ref<Event[] | null>(null)
+const totalEvents = ref(0)
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvents.value / perPage.value)
+  return page.value < totalPages
+})
+const props = defineProps({
+  page: { type: Number, required: true },
+  perPage: { type: Number, required: true },
+})
+const page = computed(() => props.page)
+const perPage = computed(() => props.perPage)
 
 onMounted(() => {
-  EventService.getEvents()
-    .then((response) => {
-      events.value = response.data
-    })
-    .catch((error) => {
-      console.error('There was an error!', error)
-    })
+  watchEffect(() => {
+    events.value = null
+    EventService.getEvents(perPage.value, page.value)
+      .then((response) => {
+        events.value = response.data
+        totalEvents.value = response.headers['x-total-count']
+      })
+      .catch((error) => {
+        console.error('There was an error!', error)
+      })
+  })
 })
 </script>
 
@@ -28,6 +43,24 @@ onMounted(() => {
       <EventInfo :event="event" />
     </div>
   </div>
+
+  <div class="pagination">
+    <RouterLink
+      id="page-prev"
+      :to="{ name: 'event-list-view', query: { page: page - 1, perPage: perPage } }"
+      rel="prev"
+      v-if="page != 1"
+      >&#60; Prev Page</RouterLink
+    >
+
+    <RouterLink
+      id="page-next"
+      :to="{ name: 'event-list-view', query: { page: page + 1, perPage: perPage } }"
+      rel="next"
+      v-if="hasNextPage"
+      >Next Page &#62;</RouterLink
+    >
+  </div>
 </template>
 
 <style scoped>
@@ -35,5 +68,20 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
