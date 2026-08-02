1bc269c6ac7235ad87538cfa15eb280defa100d2
Kha <khakee1234@gmail.com>
2026-08-02 13:25:48 +0700

    3

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-  <div id="layout">
+  <div class="text-center font-sans text-gray-700 antialiased">
     <header>
       <div id="flashMessage" v-if="message">
         <h4>{{ message }}</h4>
@@ -27,14 +27,6 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-#layout {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 nav {
   padding: 30px;
 }
