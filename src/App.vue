5d5b65590f78ae0690ba3237a57431fd85906713
Kha <khakee1234@gmail.com>
2026-08-02 16:13:00 +0700

    10.7

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from '@/stores/message'
 import { storeToRefs } from 'pinia'
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 
 const store = useMessageStore()
 const { message } = storeToRefs(store)
 </script>
 
 <template>
+  <SpeedInsights />
   <div class="text-center font-sans text-gray-700 antialiased">
     <header>
       <div id="flashMessage" class="animate-fade" v-if="message">
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics'
 
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
 
 const app = createApp(App)
+
+inject()
 
 app.use(createPinia())
 app.use(router)
