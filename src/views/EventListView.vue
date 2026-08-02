ea51a1db4f5d195578f89661c16d42767fc5fc64
Kha <khakee1234@gmail.com>
2026-08-02 13:18:15 +0700

    2.2

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -39,7 +39,7 @@ onMounted(() => {
 <template>
   <h1>Events For Good</h1>
 
-  <div class="events">
+  <div class="flex flex-col items-center">
     <!-- <EventCard v-for="event in events" :key="event.id" :event="event" /> -->
     <div v-for="event in events" :key="event.id" class="event-wrapper">
       <EventCard :event="event" />
@@ -67,11 +67,6 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 .pagination {
   display: flex;
   width: 290px;
